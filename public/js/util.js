4d60e15adf6531091c37d8c03c38e09acd643862
Kyo Suayan <ksuayan@gmail.com>
2013-06-30 14:52:21 -0700

    timeline fixes for MSIE10

--- public/js/util.js
+++ public/js/util.js
@@ -23,3 +23,26 @@ util.zeroFill = function(number, width) {
     }
     return number + ""; // always return a string
 };
+
+// -- http://jsfiddle.net/amustill/Bh276/1/
+Raphael.el.hoverInBounds = function(inFunc, outFunc) {
+    var inBounds = false;
+    // Mouseover function. Only execute if `inBounds` is false.
+    this.mouseover(function() {
+        if (!inBounds) {
+            inBounds = true;
+            inFunc.call(this);
+        }
+    });
+    // Mouseout function
+    this.mouseout(function(e) {
+        var x = e.offsetX || e.clientX,
+            y = e.offsetY || e.clientY;
+
+        // Return `false` if we're still inside the element's bounds
+        if (this.isPointInside(x, y)) return false;
+        inBounds = false;
+        outFunc.call(this);
+    });
+    return this;
+};
