2458877dd560c9aa1e73c7459c3d693d0fa80c86
Kyo Suayan <ksuayan@gmail.com>
2013-10-29 22:49:13 -0700

    cors for all

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ var init = function() {
 
 app.configure(init);
 
-app.all("/", function(req, res, next) {
+app.all("*", function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
