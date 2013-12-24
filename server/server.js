11008b01488770c4c6db657c358e60fc0a79ccb8
Kyo Suayan <ksuayan@gmail.com>
2013-12-23 20:57:53 -0800

    4 day caching

--- server/server.js
+++ server/server.js
@@ -12,7 +12,9 @@ var init = function () {
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'jade');
     app.locals({config: conf.app});
-    app.use(express.bodyParser({uploadDir: '/tmp/test'}));
+    app.use(express.compress());
+    // upload turned off
+    // app.use(express.bodyParser({uploadDir: '/tmp/test'}));
     app.use(express.static(path.join(__dirname, "../public")));
     app.use(express.favicon());
     app.use(express.cookieParser());
@@ -26,6 +28,18 @@ var init = function () {
 app.configure(init);
 
 app.all("*", function (req, res, next) {
+    var caching = true;
+
+    if (caching) {
+        var expire = 60 * 60 * 24 * 4; // 5 days
+
+        if (req.url.indexOf("/js/") === 0)||
+           (req.url.indexOf("/css/") === 0)||
+           (req.url.indexOf("/img/") === 0) {
+            res.setHeader("Cache-Control", "public, max-age="+expire);
+            res.setHeader("Expires", new Date(Date.now() + expire * 1000).toUTCString());
+        }
+    }
     res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
