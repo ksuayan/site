1983f3b8b06b57bb00ae6dcf7530a2b6841cbeea
Kyo Suayan <ksuayan@gmail.com>
2014-02-01 15:01:06 -0800

    cleanup logs.

--- public/js/site.js
+++ public/js/site.js
@@ -190,7 +190,23 @@ gb.ui = {
 ;gb.Namespace(gb, "gb.ui.TouchSurface");
 gb.ui.TouchSurface = new gb.Class();
 
+/**
+ * @fileOverview gb.ui.TouchSurface is a touch event detection
+ * tool for mobile devices.
+ * @author Kyo Suayan
+ * @module gb.ui.TouchSurface
+ * @example
+ * var touchSurface = new gb.ui.TouchSurface(el, yourFunction);
+ *
+ */
+
 gb.ui.TouchSurface.include({
+
+    /**
+     * @instance
+     * @param el
+     * @param callback
+     */
     init: function(el, callback) {
         "use strict";
 
@@ -214,22 +230,30 @@ gb.ui.TouchSurface.include({
         this.touchSurface.addEventListener('touchend', function(e){ that.onTouchEnd(e); }, false);
     },
 
+    /**
+     * Touch start event handler.
+     * @param e
+     */
     onTouchStart: function(e) {
-        var touchobj = e.changedTouches[0];
+        var touchObj = e.changedTouches[0];
 
         this.dir = 'none';
         this.swipeType = 'none';
-        this.startX = touchobj.pageX;
-        this.startY = touchobj.pageY;
+        this.startX = touchObj.pageX;
+        this.startY = touchObj.pageY;
         this.startTime = new Date().getTime(); // record time when finger first makes contact with surface
         this.touchHandler(e, 'none', 'start', this.swipeType, 0); // fire callback function with params dir="none", phase="start", swipetype="none" etc
         e.preventDefault();
     },
 
+    /**
+     * Touch move event handler.
+     * @param e
+     */
     onTouchMove: function(e) {
-        var touchobj = e.changedTouches[0];
-        this.distX = touchobj.pageX - this.startX; // get horizontal dist traveled by finger while in contact with surface
-        this.distY = touchobj.pageY - this.startY; // get vertical dist traveled by finger while in contact with surface
+        var touchObj = e.changedTouches[0];
+        this.distX = touchObj.pageX - this.startX; // get horizontal dist traveled by finger while in contact with surface
+        this.distY = touchObj.pageY - this.startY; // get vertical dist traveled by finger while in contact with surface
 
         if (Math.abs(this.distX) > Math.abs(this.distY)){ // if distance traveled horizontally is greater than vertically, consider this a horizontal movement
             this.dir = (this.distX < 0)? 'left' : 'right';
@@ -243,13 +267,14 @@ gb.ui.TouchSurface.include({
 
     },
 
+    /**
+     * Touch end event handler.
+     * @param e
+     */
     onTouchEnd: function(e) {
-        var touchobj = e.changedTouches[0];
-
+        var touchObj = e.changedTouches[0];
         this.elapsedTime = new Date().getTime() - this.startTime; // get time elapsed
-
         if (this.elapsedTime <= this.allowedTime) { // first condition for swipe met
-
             if (Math.abs(this.distX) >= this.threshold &&
                 Math.abs(this.distY) <= this.restraint){ // 2nd condition for horizontal swipe met
                 this.swipeType = this.dir; // set swipeType to either "left" or "right"
@@ -257,12 +282,10 @@ gb.ui.TouchSurface.include({
                 Math.abs(this.distX) <= this.restraint){ // 2nd condition for vertical swipe met
                 this.swipeType = this.dir; // set swipeType to either "top" or "down"
             }
-
         }
         // Fire callback function with params dir="left|right|up|down", phase="end", swipetype=dir etc:
         var distance = (this.dir === 'left' || this.dir === 'right') ? this.distX : this.distY;
         this.touchHandler(e, this.dir, 'end', this.swipeType, distance);
-
         e.preventDefault();
     }
 });
@@ -1167,13 +1190,9 @@ gb.ui.Stage.include({
         this.show();
         this.timeoutCycle = new gb.util.TimeOutCycle(this.intervalMS,
             function(){that.rotate();});
-
-
-        this.touchSurface = new gb.ui.TouchSurface( this.content[0] ,
+        this.touchSurface = new gb.ui.TouchSurface( this.content[0],
             function(evt, dir, phase, swipetype, distance){
-                that.onTouchEvent(evt, dir, phase, swipetype, distance);
-            });
-
+                that.onTouchEvent(evt, dir, phase, swipetype, distance);});
         $(window).resize(function(){that.fadeOut();});
         $("#stage-next").on("click", function(){that.goToNext();});
         $("#stage-prev").on("click", function(){that.goToPrevious();});
@@ -1210,8 +1229,15 @@ gb.ui.Stage.include({
         this.resizeTiles();
     },
 
+    /**
+     * onTouchEvent handler
+     * @param evt event object
+     * @param dir direction
+     * @param phase start,move,end
+     * @param swipetype
+     * @param distance
+     */
     onTouchEvent: function(evt, dir, phase, swipetype, distance) {
-        console.log("touch", dir, phase, swipetype, distance);
         if (phase !== "end")
             return;
         switch (dir) {
@@ -1369,7 +1395,6 @@ gb.ui.ContentManager.include({
         "use strict";
 
         var that = this;
-
         this.content = $(selector);
         this.visible = true;
         this.fullscreen = new gb.ui.FullScreen();
