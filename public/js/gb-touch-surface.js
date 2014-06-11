d9b6c2f15ddd0ce8ec0d497207600f3c9f22b4fa
Kyo Suayan <wsuayan@stanfordmed.org>
2014-06-11 09:42:44 -0700

    touch surface event handling tweaks ..

--- public/js/gb-touch-surface.js
+++ public/js/gb-touch-surface.js
@@ -10,9 +10,7 @@ gb.ui.TouchSurface = new gb.Class();
  * var touchSurface = new gb.ui.TouchSurface(el, yourFunction);
  *
  */
-
 gb.ui.TouchSurface.include({
-
     /**
      * @instance
      * @param el
@@ -20,25 +18,46 @@ gb.ui.TouchSurface.include({
      */
     init: function(el, callback) {
         "use strict";
+        try {
+            var that = this;
+            this.touchSurface = el;
+            this.direction = 'none';
+            this.swipeType = 'none';
+            this.startX = null;
+            this.startY = null;
+            this.distX = null;
+            this.distY = null;
+            this.threshold = 50; //required min distance traveled to be considered swipe
+            this.restraint = 50; // maximum distance allowed at the same time in perpendicular direction
+            this.allowedTime = 500; // maximum time allowed to travel that distance
+            this.elapsedTime = null;
+            this.startTime = null;
+            this.touchHandler = callback || function(evt, dir, phase, swipetype, distance){};
 
-        var that = this;
-        this.touchSurface = el;
-        this.dir = "unknown";
-        this.swipeType = "unknown";
-        this.startX = null;
-        this.startY = null;
-        this.distX = null;
-        this.distY = null;
-        this.threshold = 150; //required min distance traveled to be considered swipe
-        this.restraint = 100; // maximum distance allowed at the same time in perpendicular direction
-        this.allowedTime = 500; // maximum time allowed to travel that distance
-        this.elapsedTime = null;
-        this.startTime = null;
-        this.touchHandler = callback || function(evt, dir, phase, swipetype, distance){};
+            this.touchSurface.addEventListener('touchstart', function(e){ that.onTouchStart(e); }, false);
+            this.touchSurface.addEventListener('touchmove', function(e){ that.onTouchMove(e); }, false);
+            this.touchSurface.addEventListener('touchend', function(e){ that.onTouchEnd(e); }, false);
+        } catch(err) {}
+    },
 
-        this.touchSurface.addEventListener('touchstart', function(e){ that.onTouchStart(e); }, false);
-        this.touchSurface.addEventListener('touchmove', function(e){ that.onTouchMove(e); }, false);
-        this.touchSurface.addEventListener('touchend', function(e){ that.onTouchEnd(e); }, false);
+    /**
+     * Unbind event listeners.
+     */
+    removeEventListeners: function() {
+        try {
+            this.touchSurface.removeEventListener('touchstart');
+            this.touchSurface.removeEventListener('touchmove');
+            this.touchSurface.removeEventListener('touchend');
+        } catch(err) {}
+    },
+
+    /**
+     *
+     */
+    debug: function() {
+        console.log("----------------");
+        console.log("startX", this.startX, "startY", this.startY);
+        console.log("x", this.distX, "y", this.distY, this.axis, this.direction);
     },
 
     /**
@@ -47,14 +66,16 @@ gb.ui.TouchSurface.include({
      */
     onTouchStart: function(e) {
         var touchObj = e.changedTouches[0];
-
-        this.dir = 'none';
+        e.preventDefault();
+        this.axis = null;
+        this.direction = 'none';
         this.swipeType = 'none';
         this.startX = touchObj.pageX;
         this.startY = touchObj.pageY;
-        this.startTime = new Date().getTime(); // record time when finger first makes contact with surface
-        this.touchHandler(e, 'none', 'start', this.swipeType, 0); // fire callback function with params dir="none", phase="start", swipetype="none" etc
-        e.preventDefault();
+        // record time when finger first makes contact with surface
+        this.startTime = new Date().getTime();
+        // fire callback function with params direction="none", phase="start", swipetype="none" etc
+        this.touchHandler(e, 'none', 'start', this.swipeType, 0);
     },
 
     /**
@@ -63,19 +84,29 @@ gb.ui.TouchSurface.include({
      */
     onTouchMove: function(e) {
         var touchObj = e.changedTouches[0];
-        this.distX = touchObj.pageX - this.startX; // get horizontal dist traveled by finger while in contact with surface
-        this.distY = touchObj.pageY - this.startY; // get vertical dist traveled by finger while in contact with surface
+        e.preventDefault();
 
-        if (Math.abs(this.distX) > Math.abs(this.distY)){ // if distance traveled horizontally is greater than vertically, consider this a horizontal movement
-            this.dir = (this.distX < 0)? 'left' : 'right';
-            this.touchHandler(e, this.dir, 'move', this.swipeType, this.distX); // fire callback function with params dir="left|right", phase="move", swipetype="none" etc
-        } else { // else consider this a vertical movement
-            this.dir = (this.distY < 0)? 'up' : 'down';
-            this.touchHandler(e, this.dir, 'move', this.swipeType, this.distY); // fire callback function with params dir="up|down", phase="move", swipetype="none" etc
+        // get horizontal dist traveled by finger while in contact with surface
+        this.distX = touchObj.pageX - this.startX;
+        // get vertical dist traveled by finger while in contact with surface
+        this.distY = touchObj.pageY - this.startY;
+
+        // if distance traveled horizontally is greater than vertically, consider this a horizontal movement
+        if (!this.axis) {
+            this.axis =  (Math.abs(this.distX) > Math.abs(this.distY)) ? "horizontal" : "vertical";
         }
 
-        e.preventDefault(); // prevent scrolling when inside DIV
-
+        if (this.axis === 'horizontal') {
+            this.direction = (this.distX < 0) ? 'left' : 'right';
+            // fire callback function with params direction="left|right", phase="move", swipetype="none" etc
+            this.touchHandler(e, this.direction, 'move', this.swipeType, this.distX);
+            // else consider this a vertical movement
+        } else {
+            this.direction = (this.distY < 0) ? 'up' : 'down';
+            // fire callback function with params direction="up|down", phase="move", swipetype="none" etc
+            this.touchHandler(e, this.direction, 'move', this.swipeType, this.distY);
+        }
+        // this.debug();
     },
 
     /**
@@ -83,20 +114,26 @@ gb.ui.TouchSurface.include({
      * @param e
      */
     onTouchEnd: function(e) {
-        var touchObj = e.changedTouches[0];
-        this.elapsedTime = new Date().getTime() - this.startTime; // get time elapsed
-        if (this.elapsedTime <= this.allowedTime) { // first condition for swipe met
+        e.preventDefault();
+        // get time elapsed
+
+        this.elapsedTime = new Date().getTime() - this.startTime;
+        // first condition for swipe met
+        if (this.elapsedTime <= this.allowedTime) {
+            // 2nd condition for horizontal swipe met
             if (Math.abs(this.distX) >= this.threshold &&
-                Math.abs(this.distY) <= this.restraint){ // 2nd condition for horizontal swipe met
-                this.swipeType = this.dir; // set swipeType to either "left" or "right"
+                Math.abs(this.distY) <= this.restraint){
+                // set swipeType to either "left" or "right"
+                this.swipeType = this.direction;
+                // 2nd condition for vertical swipe met
             } else if (Math.abs(this.distY) >= this.threshold &&
-                Math.abs(this.distX) <= this.restraint){ // 2nd condition for vertical swipe met
-                this.swipeType = this.dir; // set swipeType to either "top" or "down"
+                Math.abs(this.distX) <= this.restraint){
+                // set swipeType to either "top" or "down"
+                this.swipeType = this.direction;
             }
         }
-        // Fire callback function with params dir="left|right|up|down", phase="end", swipetype=dir etc:
-        var distance = (this.dir === 'left' || this.dir === 'right') ? this.distX : this.distY;
-        this.touchHandler(e, this.dir, 'end', this.swipeType, distance);
-        e.preventDefault();
+        // Fire callback function with params direction="left|right|up|down", phase="end", swipetype=direction
+        var distance = (this.direction === 'left' || this.direction === 'right') ? this.distX : this.distY;
+        this.touchHandler(e, this.direction, 'end', this.swipeType, distance);
     }
 });
