23853b57dbec8c05702ea012928e75ba17658817
Kyo Suayan <wsuayan@stanfordmed.org>
2015-05-29 13:33:25 -0700

    d3-demo

--- public/js/d3-demo.js
+++ public/js/d3-demo.js
@@ -1,11 +1,30 @@
+var data = [{year: 2006, books: 54},
+            {year: 2007, books: 43},
+            {year: 2008, books: 41},
+            {year: 2009, books: 44},
+            {year: 2010, books: 35}];
 
-var rectDemo = d3.select("#d3-demo").
-  append("svg:svg")
-    .attr("width", 600)
-    .attr("height", 400);
+var barWidth = 40;
+var width = (barWidth + 10) * data.length;
+var height = 200;
 
-rectDemo.append("svg:rect")
-  .attr("x", 100)
-  .attr("y", 100)
-  .attr("height", 100)
-  .attr("width", 200);
+var x = d3.scale.linear()
+        .domain([0, data.length]).range([0, width]);
+var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.books; })])
+        .rangeRound([0, height]);
+
+// add the canvas to the DOM
+var barDemo = d3.select("#d3-demo")
+    .append("svg:svg")
+    .attr("width", width)
+    .attr("height", height);
+
+barDemo.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("svg:rect")
+    .attr("x", function(datum, index) { return x(index); })
+    .attr("y", function(datum) { return height - y(datum.books); })
+    .attr("height", function(datum) { return y(datum.books); })
+    .attr("width", barWidth)
+    .attr("fill", "#2d578b");
