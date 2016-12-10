d49ecffb83dd7d1b8675da8c03be22ff81d1bd74
Kyo Suayan <ksuayan@gmail.com>
2016-12-10 12:11:10 -0800

    mobile adjustments

--- public/js/dist/site.js
+++ public/js/dist/site.js
@@ -714,19 +714,14 @@ gb.ui.LazyImage.include({
                 elemTop = $(elem).offset().top,
                 elemBottom = elemTop + $(elem).height(),
                 isVisible = ((elemTop < docViewBottom) && (elemTop > docViewTop));
-                // isVisible = ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-
-            console.log("scr elTop", elemTop, "elBottom: ", elemBottom,
-                "top", docViewTop, "bottom", docViewBottom);
-
             return isVisible;
         };
-        /*
+
         var throttleScroll = function(evt){
             that.images.trigger("scrollend");
         };
-        $(window).scroll(gb.util.throttle(throttleScroll, 200));
-        */
+        $(window).scroll(gb.util.throttle(throttleScroll, 300));
+
         this.images = $(selector);
         this.images.on("scrollend", function(e){
             var target = $(e.target),
@@ -738,13 +733,9 @@ gb.ui.LazyImage.include({
                 target.attr("src", dataSrc);
                 target.off("scrollend");
                 target.removeAttr("data-src");
-                console.log("scrollend", $(this).attr("src"));
             }
         });
         $(window).scroll(function(){
-            var docViewTop = $(window).scrollTop(),
-                docViewBottom = docViewTop + $(window).height();
-            console.log("top, bottom", docViewTop, docViewBottom);
             that.images.trigger("scrollend");
         });
         this.images.trigger("scrollend");
