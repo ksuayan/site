3c4ec71fe8bb59d4b90c4c2d046c3da12cfe20ea
Kyo Suayan <ksuayan@gmail.com>
2017-10-27 16:01:53 -0700

    preload maps.json

--- loader/mongodb/db-load.sh
+++ loader/mongodb/db-load.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# mongoimport --drop --host db --db site --collection users --file /loader/users.json
+#mongoimport --drop --host db --db site --collection users --file /loader/users.json
 mongoimport --drop --host db --db site --collection pages  --file /loader/pages.json
 mongoimport --drop --host db --db site --collection jobs --file /loader/jobs.json
 mongoimport --drop --host db --db site --collection tiles --file /loader/tiles.json
 mongoimport --drop --host db --db site --collection stream --file /loader/stream.json
 mongoimport --drop --host db --db site --collection trackdbs --file /loader/trackdbs.json
 mongoimport --drop --host db --db site --collection locations --file /loader/locations.json
+mongoimport --drop --host db --db site --collection maps --file /loader/maps.json
